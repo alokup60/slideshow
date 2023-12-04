de940d4d2810249d3c3d19495bcc0712e1b6c4f4
alokup60 <alokrajsngh@gmail.com>
2023-12-04 17:02:35 +0530

    next & prev pure logic will define

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,12 +3,13 @@ import { data } from "./Data";
 
 const Slider = () => {
   const [activeImg, setActiveImg] = useState(0);
-  const nextHandler = () => {
-    setActiveImg(activeImg + 1);
-  };
   const prevHandler = () => {
-    setActiveImg(activeImg - 1);
+    setActiveImg(!activeImg ? data.length - 1 : activeImg - 1);
   };
+  const nextHandler = () => {
+    setActiveImg((activeImg + 1) % data.length);
+  };
+
   return (
     <div className="flex justify-center flex-col mx-auto items-center gap-4 mt-[4rem]">
       <h3 className="font-bold text-2xl text-orange-400">Slider</h3>
@@ -16,7 +17,7 @@ const Slider = () => {
         <button className="font-semibold text-green-500" onClick={prevHandler}>
           Prev
         </button>
-        <img src={data[activeImg]} className="w-[400px] h-[250px] rounded-md" />
+        <img src={data[activeImg]} className="w-[600px] h-[350px] rounded-md" />
         <button className="font-semibold text-green-500" onClick={nextHandler}>
           Next
         </button>
